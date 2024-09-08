28e3c67e24658bb9ee0e7ac8ad9aa4b28059a06d
henrihummal <henri.hummal@easypark.net>
2024-09-08 15:05:46 +0300

    THESIS-14 THESIS-18 THESIS-19 simplify data importing and missing value handling

---
+++ utility.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+
+def missing_index_adder(df, start_time, end_time):
+    all_time_buckets = pd.date_range(start_time, end_time, freq='h')
+    missing_timestamps = all_time_buckets.difference(df.index)
+    missing_df = pd.DataFrame(index=missing_timestamps, columns=df.columns)
+    df = pd.concat([df, missing_df])
+    return df.sort_index()
+
+def merge_one_sites_dataframes(load_df, price_df, weather_df):
+    merged_df = pd.merge(load_df, price_df, left_index=True, right_on='start_time', how='inner')
+    merged_df = pd.merge(merged_df, weather_df, left_on='start_time', right_on='start_time', how='inner')
+    merged_df.set_index('start_time', inplace=True)
+    return merged_df
+
+def load_one_sites_dataframes(site):
+    root_data_path = os.environ['ROOT_DATA_FOLDER_PATH']
+    load_energy_sum_folder_path = '/load_energy_sum'
+    prices_path = '/prices'
+    weather_path = '/weather'
+    load_df = pd.read_json(f'{root_data_path}{load_energy_sum_folder_path}/{site}.json')
+    price_df = pd.read_json(f'{root_data_path}{prices_path}/{site}.json')
+    weather_df = pd.read_json(f'{root_data_path}{weather_path}/{site}.json')
+    return load_df, price_df, weather_df
+
+def add_missing_datetimes_to_load_df(load_df):
+    load_df['time_bucket'] = pd.to_datetime(load_df['time_bucket'])
+    load_df = load_df.set_index('time_bucket')
+    load_df = missing_index_adder(load_df, load_df.index.min(), load_df.index.max())
+    return load_df
