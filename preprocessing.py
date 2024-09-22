83f4ec8583b107c1413227dff2e7a868f47d4031
henrihummal <henri.hummal@easypark.net>
2024-09-22 16:29:00 +0300

    Introduce long data format for Autogluon compatibility

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 import utility
 
-sites_with_data = {}
+sites_with_data_wide = {}
 
 def _import_data ():
     sites_ids = [2, 4, 5, 6, 12]
@@ -10,10 +10,14 @@ def _import_data ():
         load_df = utility.add_missing_datetimes_to_load_df(load_df)
         merged_df = utility.merge_one_sites_dataframes(load_df, price_df, weather_df)
         merged_df = utility.drop_redundant_merged_df_columns(merged_df)
-        sites_with_data[site_id] = merged_df
+        sites_with_data_wide[site_id] = merged_df
 
-
-def get_sites_with_data():
-    if len(sites_with_data) == 0:
+def get_sites_with_data_wide():
+    if len(sites_with_data_wide) == 0:
         _import_data()
-    return sites_with_data
+    return sites_with_data_wide
+
+def get_sites_with_data_long():
+    if len(sites_with_data_wide) == 0:
+        _import_data()
+    return utility.wide_to_long(sites_with_data_wide)
