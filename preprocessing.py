2ddac682cb974e5200b5d0a0173fedc43315849a
henrihummal <henri.hummal@easypark.net>
2024-09-22 16:33:37 +0300

    THESIS-14 fill missing load_energy_sum via linear interpolation

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import utility
+import pandas as pd
 
 sites_with_data_wide = {}
 
@@ -8,6 +9,8 @@ def _import_data ():
     for site_id in sites_ids:
         load_df, price_df, weather_df = utility.load_one_sites_dataframes(site_id)
         load_df = utility.add_missing_datetimes_to_load_df(load_df)
+        load_df['load_energy_sum'] = load_df['load_energy_sum'].apply(lambda x: int(x) if pd.notnull(x) else x)
+        load_df = load_df.interpolate()
         merged_df = utility.merge_one_sites_dataframes(load_df, price_df, weather_df)
         merged_df = utility.drop_redundant_merged_df_columns(merged_df)
         sites_with_data_wide[site_id] = merged_df
