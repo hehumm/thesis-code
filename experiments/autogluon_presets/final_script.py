338832ca689faf75807c202dbaf3d6ecf5b63f70
Henri <henri.hummal@gmail.com>
2024-11-01 19:14:46 +0200

    THESIS-31 add remote host code location path

--- experiments/autogluon_presets/final_script.py
+++ experiments/autogluon_presets/final_script.py
@@ -3,6 +3,9 @@ import os
 import experiments.autogluon_presets.model_training as model_training
 import experiments.chronos.preprocessing_autogluon as preprocessing_autogluon
 
+#common_path='/home/henri/Code/thesis-code/experiments/autogluon_presets'
+common_path='/root/code/thesis-code/experiments/autogluon_presets'
+
 # possible metrics: 'MAE', 'WAPE', 'MSE', 'RMSE'
 def train_for_one_metric(metric):
     sites_ids = [2, 4, 5, 6, 12]
@@ -13,8 +16,8 @@ def train_for_one_metric(metric):
         model_training.train_model(
             sites_data_without_covariates.get(site_id), 
             prediction_length, 
-            f'/home/henri/Code/thesis-code/experiments/autogluon_presets/models/separate/{site_id}', 
-            f'/home/henri/Code/thesis-code/experiments/autogluon_presets/predictions/separate/{site_id}.csv', 
+            f'{common_path}/models/separate/{site_id}', 
+            f'{common_path}/predictions/separate/{site_id}.csv', 
             metric
         )
     print('Best quality separate models trained and predictions made.')
@@ -24,8 +27,8 @@ def train_for_one_metric(metric):
         model_training.train_model(
             sites_data_with_covariates.get(site_id), 
             prediction_length, 
-            f'/home/henri/Code/thesis-code/experiments/autogluon_presets/models/separate_exog/{site_id}', 
-            f'/home/henri/Code/thesis-code/experiments/autogluon_presets/predictions/separate_exog/{site_id}.csv', 
+            f'{common_path}/models/separate_exog/{site_id}', 
+            f'{common_path}/predictions/separate_exog/{site_id}.csv', 
             metric
         )
     print('Best quality separate models with exogenous features trained and predictions made.')
@@ -34,8 +37,8 @@ def train_for_one_metric(metric):
     model_training.train_model(
         unified_data_without_covariates, 
         prediction_length, 
-        f'/home/henri/Code/thesis-code/experiments/autogluon_presets/models/unified', 
-        f'/home/henri/Code/thesis-code/experiments/autogluon_presets/predictions/unified.csv', 
+        f'{common_path}/models/unified', 
+        f'{common_path}/predictions/unified.csv', 
         metric
     )
     print('Best quality unified model trained and predictions made.')
@@ -44,15 +47,15 @@ def train_for_one_metric(metric):
     model_training.train_model(
         unified_data_with_covariates, 
         prediction_length, 
-        f'/home/henri/Code/thesis-code/experiments/autogluon_presets/models/unified_exog', 
-        f'/home/henri/Code/thesis-code/experiments/autogluon_presets/predictions/unified_exog.csv', 
+        f'{common_path}/models/unified_exog', 
+        f'{common_path}/predictions/unified_exog.csv', 
         metric
     )
     print('Best quality unified model with exogenous features trained and predictions made.')
     print(f'All models trained and predictions made for metric {metric}.')
 
 # Check and create directories if they do not exist
-predictions_dir = '/home/henri/Code/thesis-code/experiments/autogluon_presets/predictions'
+predictions_dir = '{common_path}/predictions'
 separate_dir = os.path.join(predictions_dir, 'separate')
 separate_exog_dir = os.path.join(predictions_dir, 'separate_exog')
 os.makedirs(separate_dir, exist_ok=True)
