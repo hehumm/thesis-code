2a8e14b78df73941631a937d6cc82da1119e7bb0
Henri <henri.hummal@gmail.com>
2024-11-22 15:05:31 +0200

    clean up and modularize code

--- experiments/autogluon_general/data_overview_plots.py
+++ experiments/autogluon_general/data_overview_plots.py
@@ -2,12 +2,27 @@ import matplotlib.pyplot as plt
 import shared.preprocessing_general as preprocessing_general
 import experiments.sarimax.preprocessing.spike_remover as spike_remover
 
-dfs = preprocessing_general.get_sites_with_data_wide()
-#dfs = spike_remover.find_spikes()
+original_dfs = preprocessing_general.get_sites_with_data_wide()
+all_sites_spikes = spike_remover.find_spikes()
 
-for site_id, df in dfs.items():
-    plt.figure(figsize=(24, 12))
-    plt.plot(df['load_energy_sum'])
-    plt.title(f'Site {site_id}')
-    plt.ylabel('Electricity Consumption (mWh)')
+def plot_vanilla_data():
+
+    for site_id, df in original_dfs.items():
+        plt.figure(figsize=(24, 12))
+        plt.plot(df['load_energy_sum'])
+        plt.title(f'Site {site_id}')
+        plt.ylabel('Electricity Consumption (mWh)')
     plt.show()
+
+def plot_spikes():
+    for site_id, spike in all_sites_spikes.items():
+        plt.figure(figsize=(24, 12))
+        plt.plot(original_dfs[site_id]['load_energy_sum'], label='Original')
+        plt.plot(spike['load_energy_sum'], label='Spikes')
+        plt.title(f'Site {site_id}')
+        plt.ylabel('Electricity Consumption (mWh)')
+        plt.legend(loc='upper left')
+        plt.show()
+
+plot_vanilla_data()
+plot_spikes()
--- experiments/sarimax/preprocessing/spike_remover.py
+++ experiments/sarimax/preprocessing/spike_remover.py
@@ -1,7 +1,4 @@
-import pandas as pd
-import numpy as np
 import shared.preprocessing_general as preprocessing_general
-import matplotlib.pyplot as plt
 
 def _find_spikes_for_one_site(df, column, threshold=0.95):
     threshold_value = df[column].quantile(threshold)
@@ -15,15 +12,3 @@ def find_spikes(spike_threshold=0.95):
         spike = _find_spikes_for_one_site(df, 'load_energy_sum', threshold=0.95)
         spikes[site_id] = spike
     return spikes
-
-global_spikes = find_spikes()
-dfs = preprocessing_general.get_sites_with_data_wide()
-
-for site_id, spike in global_spikes.items():
-    plt.figure(figsize=(24, 12))
-    plt.plot(dfs[site_id]['load_energy_sum'], label='Original')
-    plt.plot(spike['load_energy_sum'], label='Spikes')
-    plt.title(f'Site {site_id}')
-    plt.ylabel('Electricity Consumption (mWh)')
-    plt.legend(loc='upper left')
-    plt.show() 
