f05542d45bcad97941ee73c775f5ec2bffeb0f84
Henri <henri.hummal@gmail.com>
2024-12-16 19:06:31 +0200

    clean up sarimax parameters search

--- experiments/final/skforecast/execution_methods.py
+++ experiments/final/skforecast/execution_methods.py
@@ -7,30 +7,31 @@ import experiments.final.autogluon.preprocessing_autogluon as preprocessing_auto
 import experiments.final.final_shared as final_shared
 import shared.shared_utility as shared_utility
 
+def _get_params_file_path():
+    output_dir = '/home/henri/Code/thesis-code/sk_forecast_forecasts'
+    os.makedirs(output_dir, exist_ok=True)
+    return os.path.join(output_dir, 'sarimax_params_finding_time_logs.txt')
+
 def _generate_sarimax_orders(site, site_main, site_covariates):
+    output_file = _get_params_file_path()
+    with open(output_file, 'a') as f:
+        f.write(f'Best params search for site {site} started at {datetime.now()}\n')
+
     y = pd.Series(data=site_main.set_index('timestamp')['target']).asfreq('h')
     exog = site_covariates.set_index('timestamp').drop(columns=['item_id']).asfreq('h')
     best_params = tuner.perform_grid_search(config.param_grids.get(site), y, exog)
     best_params.to_excel(f'best_params_{site}.xlsx')
 
-def fit_predict():
-    output_dir = '/home/henri/Code/thesis-code/sk_forecast_forecasts'
-    os.makedirs(output_dir, exist_ok=True)
-    output_file = os.path.join(output_dir, 'skforecast_time_logs.txt')
+    with open(output_file, 'a') as f:
+        f.write(f'Best params search for site {site} ended at {datetime.now()}\n')
 
+def fit_predict():
     sites_main = preprocessing_autogluon.get_sites_independent_dfs_only_main()
     sites_only_covariates = preprocessing_autogluon.get_sites_independent_dfs_only_covariates()
 
-    #add train-test split
-
     for site in shared_utility.sites:
-        with open(output_file, 'a') as f:
-            f.write(f'Best params search for site {site} started at {datetime.now()}\n')
-
         pred_len = final_shared.configuration.get('prediction_length')
         site_main_train = sites_main[site][:-pred_len]
         site_covariates_train = sites_only_covariates[site][:-pred_len]
-        _generate_sarimax_orders(site, site_main_train, site_covariates_train)
-
-        with open(output_file, 'a') as f:
-            f.write(f'Best params search for site {site} ended at {datetime.now()}\n')
+        #_generate_sarimax_orders(site, site_main_train, site_covariates_train)
+        
