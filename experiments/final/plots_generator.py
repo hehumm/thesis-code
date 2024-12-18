b2d2524b473166a740c11cef60f2b2f22d687db9
Henri <henri.hummal@gmail.com>
2024-12-19 00:42:51 +0200

    publish results' visualizations

--- experiments/final/plots_generator.py
+++ experiments/final/plots_generator.py
@@ -14,16 +14,16 @@ def plot_data():
         values = data[site]
         plt.figure(figsize=(24, 12))
         plt.plot(values['actual'].index[-(final_shared.configuration.get('prediction_length')*2):],
-                 values['actual']['target'][-(final_shared.configuration.get('prediction_length')*2):], label='Mõõdetud tarbimine')
+                 values['actual']['target'][-(final_shared.configuration.get('prediction_length')*2):], label='Actual consumption')
         plt.plot(values['Theta'].index, values['Theta']['mean'], label='Theta')
         plt.plot(values['ETS'].index, values['ETS']['mean'], label='ETS')
         plt.plot(values['AutoARIMA'].index, values['AutoARIMA']['mean'], label='AutoARIMA')
         plt.plot(values['Chronos'].index, values['Chronos']['mean'], label='Chronos')
         plt.plot(values['SARIMAX'].index, values['SARIMAX']['mean'], label='SARIMAX')
         plt.plot(values['LSTM'].index, values['LSTM']['mean'], label='LSTM')
-        plt.title(f'Asukoht {site}')
-        plt.xlabel('Aeg')
-        plt.ylabel('Elektritarbimine (MWh)')
+        plt.title(f'Site {site}')
+        plt.xlabel('Time')
+        plt.ylabel('Electricity consumption (MWh)')
         plt.legend(loc='upper left')
         # plot_filename = f'plot_site_{site}.png'
         # plt.savefig(os.path.join(plots_dir, plot_filename))
